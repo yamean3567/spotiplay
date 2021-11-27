072b7a3908b2ba438a82f2289e95955729760b5d
YosefAmin <yosefamin3567@gmail.com>
2021-11-27 11:10:34 +0100

    project init

---
+++ spotiplay-webapp/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import { Route, BrowserRouter as Router} from 'react-router-dom';
+
+function App() {
+
+  return (
+    <Router>
+      <div className="App">
+        Route test    
+      </div>
+
+    </Router>
+  );
+}
+
+export default App;
