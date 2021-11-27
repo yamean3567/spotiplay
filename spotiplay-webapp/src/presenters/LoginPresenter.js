776213681ef0b3f569fd63594e106c9f8c09353d
YosefAmin <yosefamin3567@gmail.com>
2021-11-27 20:11:28 +0100

    handle navigation in presenters, not in context

--- spotiplay-webapp/src/presenters/LoginPresenter.js
+++ spotiplay-webapp/src/presenters/LoginPresenter.js
@@ -1,14 +1,22 @@
 import Login from '../components/Start/Login'
 import {useAuth} from '../contexts/auth';
-
+import { useNavigate } from 'react-router';
+import { useState } from 'react';
 const LoginPresenter = () => {
+    const [error, setError] = useState();
+    const navigate = useNavigate();
     const auth = useAuth();
-    const handleLogin = (email, password) => {
-        auth.logIn(email, password);
+    const handleLogin = async (email, password) => {
+        try {
+            await auth.logIn(email, password);
+            navigate('/home');
+        } catch(e) {
+            setError(e.message);
+        }
     }
     return (
         <div>
-            <Login logIn={(email, password) => handleLogin(email, password)} error={auth.error}/>
+            <Login logIn={(email, password) => handleLogin(email, password)} error={error}/>
         </div>
     )
 }
