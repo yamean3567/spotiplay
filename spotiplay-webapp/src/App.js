1fa4f7211d1315845ca6a580f607de8d13ca0ab2
YosefAmin <yosefamin3567@gmail.com>
2021-11-28 13:22:07 +0100

    done

--- spotiplay-webapp/src/App.js
+++ spotiplay-webapp/src/App.js
@@ -1,29 +1,27 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPresenter from './presenters/LoginPresenter'
 import AboutPresenter from './presenters/AboutPresenter'
 import RegisterPresenter from './presenters/RegisterPresenter'
 import Error from './components/Error/Error'
 import Home from './components/Home/Home'
 import RequireAuth from './components/Auth/RequireAuth';
-import { useAuth } from './contexts/auth';
 
 function App() {
-  const auth = useAuth();
 
   return (
     <Router>
       <Routes>
-        <Route path="/" element={auth.currentUser !== null ? <Navigate to="/home"/> : <LoginPresenter/>}/>
+        <Route path="/" element={<LoginPresenter/>}/>
         <Route path="/about" element={<AboutPresenter/>}/>
-        <Route path="/register" element={auth.currentUser !== null ? <Navigate to="/home"/> : <RegisterPresenter/>}/>
+        <Route path="/register" element={<RegisterPresenter/>}/>
         <Route path="/home" element={
           <RequireAuth>
             <Home />
           </RequireAuth>
         }/>
-        <Route path="/:pagename" element={<Error/>}/>  {/*catch all*/}
+        <Route path="*" element={<Error/>}/>  {/*catch all*/}
       </Routes>
     </Router>
   );
