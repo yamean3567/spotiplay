0e13844f536f227a811364163c203fc7d9fe0570
malinasdfghjkl <malin.marques@gmail.com>
2021-12-05 12:14:24 +0100

    both tracks load bug fix

--- spotiplay-webapp/src/components/Games/HigherLower/HigherLowerGame.js
+++ spotiplay-webapp/src/components/Games/HigherLower/HigherLowerGame.js
@@ -5,14 +5,14 @@ const HigherLower = (props) => {
 
     return (
         <div>
-            <div> track1 {props.track1}</div>
+            <div> {props.track1}</div>
             <br/>
-            <div> track2 {props.track2} is
+            <div> {props.track2} is
             <br/>
-            <div><button onClick={(e)=>props.higher(props.id1, props.id2)}> Higher </button></div>
+            <div><button onClick={()=>props.higher(props.id1, props.id2)}> Higher </button></div>
             <br/>
-            <div><button onClick={(e)=>props.lower(props.id1, props.id2)}> Lower </button></div>
-            on the US chart than {props.track1.track_name}
+            <div><button onClick={()=>props.lower(props.id1, props.id2)}> Lower </button></div>
+           on the US chart than {props.track1}
             </div>
             
         </div>
