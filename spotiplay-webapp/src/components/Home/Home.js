3823e89dcf778cc7745c20d8d471955dfb8a2e30
malinasdfghjkl <malin.marques@gmail.com>
2021-12-08 11:09:47 +0100

    button colours

--- spotiplay-webapp/src/components/Home/Home.js
+++ spotiplay-webapp/src/components/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import { updateRating } from '../../models/User';
 import AuthConsumer from '../../contexts/auth';
+
 const Home = () => {
     const auth = AuthConsumer();
     const navigate = useNavigate();
@@ -11,20 +12,20 @@ const Home = () => {
     }
 
     return (
-        <div>
-            Wow we're logged in and protected!
-            <div>
-                {auth.currentUser !== null && <button onClick={() => handleLogout()}>Logout</button>}
+        <div className="h-screen flex flex-col justify-center items-center bg-white">
+            <div className="rounded-lg text-white">
+            <div className="p-3.5 bg-green-900 rounded-lg">
+                {auth.currentUser !== null && <button className="font-bold py-3 rounded" onClick={() => handleLogout()} >Logout</button>}
             </div>
-            Update rating test 
-            <div>
-                <button onClick={() => {updateRating(auth.currentUser.uid, 2000);}}>change rating once</button>
+            <div className="p-3.5 bg-green-800 rounded-lg">
+                <button className="font-bold py-3 rounded" onClick={() => {updateRating(auth.currentUser.uid, 2000);}}>change rating once</button>
             </div>
-            <div>
-                <button onClick={() => navigate('/home/guessthelyrics')}>Guess the lyrics</button>
+            <div className="p-3.5 bg-green-700 rounded-lg">
+                <button className="font-bold py-3 rounded" onClick={() => navigate('/home/guessthelyrics')}>Guess the lyrics</button>
             </div>
-            <div>
-                <button onClick={() => navigate('/home/higherlower')}>Higher or lower</button>
+            <div className="p-3.5 bg-green-600 rounded-lg">
+                <button className="font-bold py-3 rounded" onClick={() => navigate('/home/higherlower')}>Higher or lower</button>
+            </div>
             </div>
         </div>
     )
