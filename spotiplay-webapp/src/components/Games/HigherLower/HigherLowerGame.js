bd23ce47b98d94a7c7d2b6da8add2259b4c478ac
malinasdfghjkl <malin.marques@gmail.com>
2021-12-10 00:15:02 +0100

    final

--- spotiplay-webapp/src/components/Games/HigherLower/HigherLowerGame.js
+++ spotiplay-webapp/src/components/Games/HigherLower/HigherLowerGame.js
@@ -6,15 +6,15 @@ const HigherLower = (props) => {
     return (
         <div className="flex min-h-screen bg-gradient-to-b from-black via-black to-green-900 text-gray-300 font-mono flex-col flex-row">
             <div className=' flex flex-col flex-grow flex-grow'>
-                <div className='bg-red-500'>
+                <div className=''>
                     Score: {props.currentScore}
                 </div>
 
-                <div className="bg-yellow-500 flex flex-row">
+                <div className=" flex flex-row items-stretch">
 
-                    <div className=" bg-blue-500 text-center p-2 text-sm font-bold text-white flex-grow"> {props.track1}  by {props.artist1}</div>
+                    <div className=" text-center p-2 text-sm font-bold text-white flex-grow"> {props.track1}  by {props.artist1}</div>
 
-                    <div className="bg-purple-500 text-center p-2 text-sm font-bold text-white inset-y-2 right-2 flex-grow"> {props.track2} by {props.artist2} is
+                    <div className="text-center p-2 text-sm font-bold text-white inset-y-2 right-2 flex-grow"> {props.track2} by {props.artist2} is
                         <div><button className="p-3 bg-green-600 rounded-lg m-3 p-2 text-sm font-bold text-white hover:bg-green-900" onClick={() => props.higher(props.id1, props.id2)}> Higher </button></div>
                         <div><button className="p-3 bg-gray-600 rounded-lg m-3 p-2 text-sm font-bold text-white hover:bg-gray-900" onClick={() => props.lower(props.id1, props.id2)}> Lower </button></div>
                         on the US chart than {props.track1}
