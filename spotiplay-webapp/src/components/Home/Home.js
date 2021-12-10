4c7d7aa6845a6c51a6a63031c3ad1b146a73b426
malinasdfghjkl <malin.marques@gmail.com>
2021-12-10 09:33:25 +0100

    leaderboard navigation home page

--- spotiplay-webapp/src/components/Home/Home.js
+++ spotiplay-webapp/src/components/Home/Home.js
@@ -15,7 +15,7 @@ const Home = () => {
         <div className="h-screen flex flex-col justify-center items-center bg-white">
             <div className="w-1/3 bg-white shadow-md px-8 pt-6 pb-16 text-white">
             <div className="p-1 bg-green-800 rounded-lg m-3 text-center hover:bg-green-900 ">
-                <button className="font-bold py-3 rounded" onClick={() => {updateRating(auth.currentUser.uid, 2000);}}>change rating once</button>
+                <button className="font-bold py-3 rounded" onClick={() => navigate('/home/leaderboard')}>Leader Board</button>
             </div>
             <div className="p-1 bg-green-700 rounded-lg m-3 text-center hover:bg-green-900">
                 <button className="font-bold py-3 rounded" onClick={() => navigate('/home/guessthelyrics')}>Guess the Lyrics</button>
