bacd2048b2ac57d165874f0c327c9dd684f4a672
Lukas <lukasmat@kth.se>
2021-12-10 23:02:32 +0100

    button fix -user eval

--- spotiplay-webapp/src/components/Games/Leaderboard/Leaderboard.js
+++ spotiplay-webapp/src/components/Games/Leaderboard/Leaderboard.js
@@ -10,8 +10,8 @@ const DisplayLeaderboard = (props) => {
     return (
         <div className="grid place-items-center">
             <ul id="tabs" className="flex cursor-pointer">
-                <li className={activeTab === "Tab 1" ? "py-2 px-6 bg-green-400 rounded-t-lg text-black" : "py-2 px-6 bg-white rounded-t-lg text-gray-500 bg-gray-200"}><a href="#tab1" onClick={e => handleClick(e, "Tab 1")} key={"tab1"}>Guess the lyrics</a></li>
-                <li className={activeTab === "Tab 2" ? "py-2 px-6 bg-green-400 rounded-t-lg text-black" : "py-2 px-6 bg-white rounded-t-lg text-gray-500 bg-gray-200"}><a href="#tab2" onClick={e => handleClick(e, "Tab 2")} key={"tab2"}>Higher or Lower</a></li>
+                <li className={activeTab === "Tab 1" ? "py-2 px-6 bg-green-400 rounded-t-lg text-black" : "py-2 px-6 bg-white rounded-t-lg text-gray-500 bg-gray-200"} onClick={e => handleClick(e, "Tab 1")}><a href="#tab1" key={"tab1"}>Guess the lyrics</a></li>
+                <li className={activeTab === "Tab 2" ? "py-2 px-6 bg-green-400 rounded-t-lg text-black" : "py-2 px-6 bg-white rounded-t-lg text-gray-500 bg-gray-200"} onClick={e => handleClick(e, "Tab 2")}><a href="#tab2" key={"tab2"}>Higher or Lower</a></li>
             </ul>
             <div id="tab-contents">
                 <div id="first" className={activeTab === "Tab 1" ? "p-4" : "hidden p-4"}>
