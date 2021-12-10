a83719472f5b89f3ad72beb900cc44678d0efeae
malinasdfghjkl <malin.marques@gmail.com>
2021-12-10 10:36:44 +0100

    text visibility

--- spotiplay-webapp/src/components/Games/HigherLower/HigherLowerStart.js
+++ spotiplay-webapp/src/components/Games/HigherLower/HigherLowerStart.js
@@ -6,8 +6,10 @@ const HigherLowerStart = (props) => {
         <div className='min-h-screen flex flex-col justify-center items-center bg-gradient-to-t from-gray-900 to-black'>
         <div className="grid place-items-center">
             <button className={props.color + background + " text-white font-bold py-4 px-10 rounded"} disabled={props.disabled} onClick={() => props.startGame()}>{!props.disabled ? "START" : "Starting in " + props.time}</button>
-            Guess which song is higher or lower on the US top 100!
         </div>
+        <div className="text-gray-300 font-mono">
+            <br/>
+        Guess which song is higher or lower on the US top 100!</div>
     </div>)
 }
 
